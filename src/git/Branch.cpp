d52c3158a80106737afbfd77f7ad071bbd9acb54
rizfi ferdiansyah <rizfiferdian1@gmail.com>
2026-07-19 10:47:57 +0700

    feat(branch): add branch constructor

--- src/git/Branch.cpp
+++ src/git/Branch.cpp
@@ -1,3 +1,21 @@
 #include "git/Repository.hpp"
+#include <git2/branch.h>
+#include <git2/refs.h>
+#include <git2/types.h>
+#include <stdexcept>
 
-std::string Branch::name() const { return ""; };
+Branch::Branch(git_reference *ref) {
+  if (git_reference_is_branch(ref) == 0) {
+    throw std::runtime_error("Reference is not branch");
+  }
+  const char *shortname;
+  git_branch_name(&shortname, ref);
+
+  _name = git_reference_name(ref);
+  _shortname = shortname;
+  _is_head = git_branch_is_head(ref);
+}
+
+std::string Branch::name() const { return _name; };
+std::string Branch::shortname() const { return _shortname; };
+bool Branch::isHead() const { return _is_head; };
--- src/git/Branch.hpp
+++ src/git/Branch.hpp
@@ -1,11 +1,15 @@
+#include <git2/types.h>
 #include <string>
 
 class Branch {
 public:
+  Branch(git_reference *ref);
   std::string name() const;
+  std::string shortname() const;
   bool isHead() const;
 
 private:
   std::string _name;
+  std::string _shortname;
   bool _is_head;
 };
