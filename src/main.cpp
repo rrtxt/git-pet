1505e95456a4c5b453b1b7f7cffcb568c6c2ef3a
rrtxt <rizfiferdian1@gmail.com>
2026-07-19 16:30:00 +0700

    feat(widget): add mock pet widget

--- src/main.cpp
+++ src/main.cpp
@@ -32,32 +32,15 @@ std::string exec(const std::string &cmd) {
 
 int main() {
   GitLibrary git;
-  cout << "git" << endl;
 
   std::filesystem::path path = std::filesystem::current_path();
   Repository repo(path);
 
-  cout << "repo" << endl;
   std::vector<Commit> commits = repo.history();
-
-  cout << "commit" << endl;
   Branch branch = repo.currentBranch();
 
-  cout << "branch" << endl;
-  /* std::cout << "Current Branch: " << branch.shortname() << std::endl;
-  std::cout << "Total Commit: " << commits.size() << std::endl;
-  std::cout << "Last Commit: " << commits[0].sorthash() << std::endl; */
-  // std::cout << "Is HEAD: " << branch.isHead() << std::endl;
-  /* for (Commit commit : commits) {
-    std::cout << "commit " << commit.hash() << std::endl;
-    std::cout << "Author: " << commit.author() << std::endl;
-    std::cout << "Date': " << commit.date() << std::endl;
-    std::cout << "Message: \n" << commit.message() << std::endl;
-  } */
-
-  Element card = GitCard(repo);
-  cout << "card" << endl;
-  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(card));
+  Element card = GitCard(repo) | border;
+  auto screen = Screen::Create(Dimension::Fit(card), Dimension::Full());
 
   Render(screen, card);
   screen.Print();
--- src/ui/widgets/GitCard.cpp
+++ src/ui/widgets/GitCard.cpp
@@ -1,20 +1,50 @@
 #include <format>
+#include <ftxui/dom/canvas.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <screen/ui/widgets/GitCard.hpp>
 
 using namespace ftxui;
 using namespace std;
-
+Element Pixel(Color color) { return text("  ") | bgcolor(color); }
 Element GitCard(const Repository &repo) {
   Branch branch = repo.currentBranch();
   int commitCount = repo.commitCount();
   Commit head = repo.head();
 
-  Element card = vbox({
-      text(format("Current Branch: {}", branch.name())) | border,
-      text(format("Total Commit: {}", commitCount)) | border,
-      text(format("Last Commit: {}", head.sorthash())) | border,
+  Canvas c(40, 20);
+
+  c.DrawPoint(5, 5, false);
+  c.DrawPointLine(0, 0, 20, 10);
+  c.DrawBlockCircle(20, 10, 5);
+
+  Element image = canvas(c);
+  Element pet = vbox({
+      hbox({
+          Pixel(Color::Black),
+          Pixel(Color::Red),
+          Pixel(Color::Red),
+          Pixel(Color::Black),
+      }),
+      hbox({
+          Pixel(Color::Red),
+          Pixel(Color::White),
+          Pixel(Color::White),
+          Pixel(Color::Red),
+      }),
+      hbox({
+          Pixel(Color::Black),
+          Pixel(Color::Red),
+          Pixel(Color::Red),
+          Pixel(Color::Black),
+      }),
   });
+
+  Element gitStats = vflow({
+      text(format("Current Branch: {}", branch.shortname())),
+      text(format("Total Commit: {}", commitCount)),
+      text(format("Last Commit: {}", head.sorthash())),
+  });
+  Element card = vflow({center(pet) | flex, filler(), gitStats | flex});
 
   return card;
 }
